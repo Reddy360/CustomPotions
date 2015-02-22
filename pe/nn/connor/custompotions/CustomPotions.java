0d33941a9d9283cd3db975bb0be920fad987e6bb
Connor Graham <thecg1997@gmail.com>
2015-02-22 18:24:44 +0000

    Added custompotions command (only with help subcommand)

--- pe/nn/connor/custompotions/CustomPotions.java
+++ pe/nn/connor/custompotions/CustomPotions.java
@@ -2,11 +2,14 @@ package pe.nn.connor.custompotions;
 
 import java.util.HashMap;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffectType;
 
 public class CustomPotions extends JavaPlugin{
 	private HashMap<String, PotionEffectType> potionNames;
+	
 	@Override
 	public void onEnable() {
 		//Create Potion name list
@@ -16,5 +19,22 @@ public class CustomPotions extends JavaPlugin{
 		for(PotionEffectType type : PotionEffectType.values()){
 			potionNames.put(type.getName(), type);
 		}
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		if(command.getName().equalsIgnoreCase("custompotions")){
+			if(args.length == 1){
+				if(args[0].equalsIgnoreCase("help")){
+					sender.sendMessage("Custom Potions help");
+					sender.sendMessage("This will be changed in the release build");
+					sender.sendMessage("");
+					sender.sendMessage(label + " give potion:strength:duration (-splash)");
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
