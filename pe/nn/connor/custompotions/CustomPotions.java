29a3766fad38fa43ae5b62f3fdd301889d024614
Connor Graham <thecg1997@gmail.com>
2015-02-22 18:03:43 +0000

    Added potion names HashMap

--- pe/nn/connor/custompotions/CustomPotions.java
+++ pe/nn/connor/custompotions/CustomPotions.java
@@ -1,7 +1,20 @@
 package pe.nn.connor.custompotions;
 
+import java.util.HashMap;
+
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffectType;
 
 public class CustomPotions extends JavaPlugin{
-
+	private HashMap<String, PotionEffectType> potionNames;
+	@Override
+	public void onEnable() {
+		//Create Potion name list
+		potionNames = new HashMap<String, PotionEffectType>();
+		//Create automated system for adding new potions
+		//This allows any new potion effect to be used prior to an update
+		for(PotionEffectType type : PotionEffectType.values()){
+			potionNames.put(type.getName(), type);
+		}
+	}
 }
