3c520f3cfe257365a143861622923656cfb1ae64
Connor Graham <thecg1997@gmail.com>
2015-02-22 18:40:05 +0000

    Created logic of custompotions command

--- pe/nn/connor/custompotions/CustomPotions.java
+++ pe/nn/connor/custompotions/CustomPotions.java
@@ -1,6 +1,8 @@
 package pe.nn.connor.custompotions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -31,6 +33,26 @@ public class CustomPotions extends JavaPlugin{
 					sender.sendMessage("This will be changed in the release build");
 					sender.sendMessage("");
 					sender.sendMessage(label + " give potion:strength:duration (-splash)");
+					return true;
+				}
+			}else if(args.length >= 2){
+				if(args[0].equalsIgnoreCase("give")){
+					List<String[]> potions = new ArrayList<String[]>();
+					List<String> variables = new ArrayList<String>();
+					for(int i = 1; i < args.length; i++){
+						String arg = args[i];
+						if(arg.contains(":")){
+							String[] miniArgs = arg.split(":");
+							if(miniArgs.length == 3){
+								potions.add(miniArgs);
+							}else{
+								sender.sendMessage("Malformed potion argument: " + arg);
+								sender.sendMessage("Skipping");
+							}
+						}else if(arg.startsWith("-")){
+							variables.add(arg);
+						}
+					}
 					return true;
 				}
 			}
