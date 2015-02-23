2051d0b32de176170e9453145a7daabf571962fe
Connor Graham <thecg1997@gmail.com>
2015-02-23 01:40:12 +0000

    Added CustomPotion creation logic

--- pe/nn/connor/custompotions/CustomPotions.java
+++ pe/nn/connor/custompotions/CustomPotions.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class CustomPotions extends JavaPlugin{
 	private HashMap<String, PotionEffectType> potionNames;
-	
+	private static short potionDrinkable = 64;
+	private static short potionThrowable = 16390;
 	@Override
 	public void onEnable() {
 		//Create Potion name list
@@ -32,19 +39,38 @@ public class CustomPotions extends JavaPlugin{
 					sender.sendMessage("Custom Potions help");
 					sender.sendMessage("This will be changed in the release build");
 					sender.sendMessage("");
-					sender.sendMessage(label + " give potion:strength:duration (-splash)");
+					sender.sendMessage(label + " give name potion:strength:duration (-splash)");
 					return true;
 				}
-			}else if(args.length >= 2){
+			}else if(args.length >= 3){
 				if(args[0].equalsIgnoreCase("give")){
-					List<String[]> potions = new ArrayList<String[]>();
 					List<String> variables = new ArrayList<String>();
-					for(int i = 1; i < args.length; i++){
+					Player target = null;
+					for(Player player : Bukkit.getOnlinePlayers()){
+						if(player.getName().equalsIgnoreCase(args[1])){
+							target = player;
+						}
+					}
+					if(target == null){
+						sender.sendMessage("Player is not online");
+						return true;
+					}
+					ItemStack item = new ItemStack(Material.POTION, 1);
+					PotionMeta meta = (PotionMeta) item.getItemMeta();
+					for(int i = 2; i < args.length; i++){
 						String arg = args[i];
 						if(arg.contains(":")){
 							String[] miniArgs = arg.split(":");
 							if(miniArgs.length == 3){
-								potions.add(miniArgs);
+								if(potionNames.containsKey(miniArgs[0])){
+									PotionEffectType type = potionNames.get(miniArgs[0]);
+									int duration = Integer.parseInt(miniArgs[1]) * 20;
+									int amplifier = Integer.parseInt(miniArgs[2]);
+									meta.addCustomEffect(new PotionEffect(type, duration, amplifier), true);
+								}else{
+									sender.sendMessage("Unknown potion name: " + miniArgs[0]);
+									sender.sendMessage("Ignoring");
+								}
 							}else{
 								sender.sendMessage("Malformed potion argument: " + arg);
 								sender.sendMessage("Skipping");
@@ -53,6 +79,15 @@ public class CustomPotions extends JavaPlugin{
 							variables.add(arg);
 						}
 					}
+					
+					short id = potionDrinkable;
+					if(variables.contains("-splash")){
+						id = potionThrowable;
+					}
+					item.setDurability(id);
+					item.setItemMeta(meta);
+					target.getInventory().addItem(item);
+					target.sendMessage("You have been given a Custom Potion :D");
 					return true;
 				}
 			}
